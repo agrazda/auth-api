7451458a26903ad7aec68cc55d5afd7f59d2c04c
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-10-28 14:16:50 -0700

    just saving

--- starter-code/auth-server/src/server.js
+++ starter-code/auth-server/src/server.js
@@ -10,8 +10,8 @@ const morgan = require('morgan');
 const errorHandler = require('./error-handlers/500.js');
 const notFoundHandler = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes/routes.js');
-
 const logger = require('./auth/middleware/logger.js');
+
 
 const v1Routes = require('../src/auth/routes/v1');
 
